cfb20062fef3c883623c226dff88fa63a57ebfe2
r3veal <79401825+r3veal@users.noreply.github.com>
2022-01-20 17:15:22 +0200

    script 01

---
+++ script.sh
@@ -0,0 +1,9 @@
+# This script the files in a directory to sequential numbers.
+# https://stackoverflow.com/questions/3211595/renaming-files-in-a-folder-to-sequential-numbers
+
+a=1
+for i in *.jpg; do
+  new=$(printf "%04d.jpg" "$a") #04 pad to length of 4
+  mv -i -- "$i" "$new"
+  let a=a+1
+done
